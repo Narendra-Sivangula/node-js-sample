525b41dfec04d9329726ad9efa7a984b5bde7f8d
Narendra Sivangula <narendrakumarsivangula@gmail.com>
2026-01-25 12:59:14 +0530

    Clean up blank lines in index.js
    Removed unnecessary blank lines in index.js
    
    Signed-off-by: Narendra Sivangula <narendrakumarsivangula@gmail.com>

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
-
-
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
