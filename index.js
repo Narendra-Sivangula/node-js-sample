3606e8f26e051e93d8784ae87879c77845029b33
Narendra Sivangula <narendrakumarsivangula@gmail.com>
2026-01-31 11:14:23 +0530

    Update index.js- Full details
    Signed-off-by: Narendra Sivangula <narendrakumarsivangula@gmail.com>

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 
-// Automated using ArgoCD And included image_digest
+// Automated using ArgoCD And included image_digest - Latest
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
