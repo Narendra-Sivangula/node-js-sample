d90e39c69b9f55c96620504ebe8af2780901d31a
Narendra Sivangula <narendrakumarsivangula@gmail.com>
2026-01-31 11:07:16 +0530

    Update index.js - with image_digest
    Signed-off-by: Narendra Sivangula <narendrakumarsivangula@gmail.com>

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 
-// Automated using ArgoCD
+// Automated using ArgoCD And included image_digest
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
