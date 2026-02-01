f25723911e3eb9f569d636efc40c5dd9d7d11ffb
Narendra Sivangula <narendrakumarsivangula@gmail.com>
2026-02-01 09:31:38 +0530

    Update index.js - Still Testing
    Signed-off-by: Narendra Sivangula <narendrakumarsivangula@gmail.com>

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var app = express()
 
 // Automated using ArgoCD And included image_digest - Latest
 // For Testing
-// Ultimate Working ArgoCD Testing End-To-End Final
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
