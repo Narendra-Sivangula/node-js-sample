1f1b95c4b672fc577e32e3f52c6f78bc589291f8
Narendra Sivangula <narendrakumarsivangula@gmail.com>
2026-02-14 20:50:11 +0530

    Update Crashloop Testing Simulation version
    Signed-off-by: Narendra Sivangula <narendrakumarsivangula@gmail.com>

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 
-// Crashloop Testing Simulation - version01
+// Crashloop Testing Simulation - version02
 if (process.env.CRASH_ON_START === 'true') {
   console.error('CRASH_ON_START=true, exiting for controlled crash-loop test')
   process.exit(1)
